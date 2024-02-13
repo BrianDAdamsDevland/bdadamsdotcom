9c3c9a0e4ac26600d3029a482b87f4091c5b4caf
Brian D. Adams <briandadams@gmail.com>
2024-02-13 15:25:50 -0600

    RSS support for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.bdadams.com",
   integrations: [vue(), mdx()],
   markdown: {
     shikiConfig: {
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+const blog = await getCollection('blog');
+
+export function GET(context) {
+  return rss({
+    title: 'Brian D. Adams\' Blog',
+    description: 'Musings on programming, management, and whatever else I think up!',
+    site: context.site,
+    items: blog.map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.date,
+        link: `/blog/${post.slug}/`,
+      })),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
+}
