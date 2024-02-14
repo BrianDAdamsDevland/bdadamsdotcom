c9838546074f8e9e0249f21f1c44644b13659e73
Brian D. Adams <briandadams@gmail.com>
2024-02-14 14:05:50 -0600

    Add sitemap and robots for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bdadams.com",
-  integrations: [vue(), mdx()],
+  integrations: [vue(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       wrap: true
