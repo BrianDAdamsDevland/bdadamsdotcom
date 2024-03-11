32c09da66956d958f700ede5783e180b96d49dbc
Brian D. Adams <briandadams@gmail.com>
2024-03-11 11:08:16 -0500

    Update Sentry project name

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,9 +19,9 @@ export default defineConfig({
     sentry({
       dsn: "https://c0923b76e81cff946429e3533e2a3ff1@o4506892850233344.ingest.us.sentry.io/4506892851806208",
       sourceMapsUploadOptions: {
-        project: "javascript-astro",
+        project: "bdadamsdotcom",
         authToken: SENTRY_AUTH_TOKEN,
-      },
+      }
     }),
     spotlightjs(),
   ],
